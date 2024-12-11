d70e2c1e5f9a2c2d4f1a69fb345816b4765f4bc1
hkhb <h14hbk@gmail.com>
2024-12-11 16:20:51 +0900

    Dec11 commit

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,31 @@
 class Schedule < ApplicationRecord
   validates :user_id, :name, :start_time, :end_time, presence: true
+
+  def create
+    @schedule = Schedule.new(
+                params.require(:schedule).permit(:name, :event, :start_time, :end_time)
+                )
+    @schedule.user_id = @current_user.id
+
+    if @schedule.save
+      return true
+    else
+      return 
+    end
+end
+
+  def update
+    @schedule = Schedule.find_by(id: params[:id])
+    @schedule.start_time.in_time_zone("Tokyo")
+
+    schedule_params = params.require(:schedule).permit(:name, :event, :start_time, :end_time)
+
+    Rails.logger.debug "Schedule Params: #{schedule_params.inspect}"
+
+    if @schedule.update(schedule_params)
+      return true
+    else
+      return fl
+    end
+end
 end
