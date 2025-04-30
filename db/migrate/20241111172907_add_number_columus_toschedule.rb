a2a2008c240d006870ed4fa8bb265ba55a4f8bda
hkhb <h14hbk@gmail.com>
2025-04-30 23:33:28 +0900

    バグを修正

--- db/migrate/20241111172907_add_number_columus_toschedule.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberColumusToschedule < ActiveRecord::Migration[7.2]
-  def change
-    add_column :schedules, :number, :integer 
-  end
-end
--- db/migrate/20241111173214_add_number_columu_toschedule.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberColumuToschedule < ActiveRecord::Migration[7.2]
-  def change
-    add_column :schedules, :number, :integer
-  end
-end
